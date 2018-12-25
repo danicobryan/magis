fbdb58e2ac5f3342970eca107b1b3f1443d6bd3d
Donovan Moini <donovanmoini@gmail.com>
2018-12-24 21:02:01 -0800

    Used more readable version that allows for easier sections to disregard

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,27 +1,39 @@
-$(document).ready(function(){
-  	// Add smooth scrolling to all links
-  	$("a").on('click', function(event) {
+$(document).ready(function() {
 
-
-		// Make sure this.hash has a value before overriding default behavior
-
-		if ((this.hash !== "") && (this.hash !== "#events-carousel")) {
-	  		// Prevent default anchor click behavior
-	  		event.preventDefault();
-
-	  		// Store hash
-	  		var hash = this.hash;
-
-	  		// Using jQuery's animate() method to add smooth page scroll
-	  		// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-	  		$('html, body').animate({
-				scrollTop: $(hash).offset().top
-	  		}, 800, function(){
-   
-			// Add hash (#) to URL when done scrolling (default click behavior)
-				window.location.hash = hash;
-	  		});
-		} // End if
-		
-  	});
+    // All credit for smooth scrolling code goes to https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    // Select all links with hashes
+    $("a[href*='#']")
+        // Remove links that don"t actually link to anything
+		.not("[href='#home-carousel']")
+		.not("[href='#service-carousel']")
+		.not("[href='#events-carousel']")
+		.not("[href='#join-carousel']")
+        .click(function(event) {
+            // On-page links
+            if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") &&
+                location.hostname === this.hostname) {
+                // Figure out element to scroll to
+                let target = $(this.hash);
+                target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $("html, body").animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+                        // Callback after animation
+                        // Must change focus
+                        let $target = $(target);
+                        $target.focus();
+                        if ($target.is(':focus')) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
 });
